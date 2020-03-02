422170a794013734328c3a52125982425c2a89a1
FrancisChung <francis.c.chung@gmail.com>
2020-03-02 18:30:47 +1100

    EF Migrations "Todos"

--- CaWorkshop.WebUI/Data/Migrations/20200302072905_Todos.Designer.cs
+++ CaWorkshop.WebUI/Data/Migrations/20200302072905_Todos.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using CaWorkshop.WebUI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CaWorkshop.WebUI.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200302072905_Todos")]
+    partial class Todos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
