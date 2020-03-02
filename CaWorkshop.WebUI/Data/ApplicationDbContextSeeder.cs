b4865c75f8a2470bcd939cfdda56cc33406c1ac6
FrancisChung <francis.c.chung@gmail.com>
2020-03-02 19:49:04 +1100

    Initial Seed data for TodoLists

---
+++ CaWorkshop.WebUI/Data/ApplicationDbContextSeeder.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CaWorkshop.WebUI.Models;
+
+namespace CaWorkshop.WebUI.Data
+{
+    public static class ApplicationDbContextSeeder
+    {
+        public static void Seed(ApplicationDbContext context)
+        {
+            if (context.TodoLists.Any())
+                return;
+
+            context.TodoLists.Add(
+                new TodoList
+                {
+                    Title = "Death List Five",
+                    Items = new List<TodoItem>
+                    {
+                        new TodoItem
+                        {
+                            Title = "O-Ren Ishii",
+                            Done = true
+                        },
+                        new TodoItem
+                        {
+                            Title = "Vernita Green",
+                            Done = true
+                        },
+                        new TodoItem
+                        {
+                            Title = "Budd",
+                            Done = true
+                        },
+                        new TodoItem
+                        {
+                            Title = "Ellie Driver"
+                        },
+                        new TodoItem
+                        {
+                            Title = "Bill"
+                        }
+                    }
+                }
+            );
+
+            context.SaveChanges();
+
+        }
+    }
+}
