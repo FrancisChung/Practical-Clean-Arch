b0076f58b8a5969ad34a49b6a3b6d75c29e57d06
Francis Chung <francis.c.chung@gmail.com>
2020-03-26 06:35:39 +1100

    New NS CaWorkshop.Domain.Entities

--- CaWorkshop.Domain/Entities/PriorityLevel.cs
+++ CaWorkshop.Domain/Entities/PriorityLevel.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CaWorkshop.WebUI.Models
-{
+namespace CaWorkshop.Domain.Entities
     public enum PriorityLevel
     {
         None,
--- CaWorkshop.Domain/Entities/TodoItem.cs
+++ CaWorkshop.Domain/Entities/TodoItem.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CaWorkshop.WebUI.Models
+namespace CaWorkshop.Domain.Entities
 {
     public class TodoItem
     {
