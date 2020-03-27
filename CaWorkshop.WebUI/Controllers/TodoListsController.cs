54b4c6008578403fba8c65bb92edc0fcc5aebe52
Francis Chung <francis.c.chung@gmail.com>
2020-03-27 16:28:59 +1100

    Remove obsolete Models reference

--- CaWorkshop.WebUI/Controllers/TodoListsController.cs
+++ CaWorkshop.WebUI/Controllers/TodoListsController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CaWorkshop.WebUI.Data;
-using CaWorkshop.WebUI.Models;
 
 
 namespace CaWorkshop.WebUI.Controllers
