765d5b8e9d9c89d97b75daa428c4ae7a8f9775a2
Francis Chung <francis.c.chung@gmail.com>
2020-03-28 05:36:37 +1100

    New IApplicationDBContext Interface

---
+++ CaWorkshop.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CaWorkshop.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaWorkshop.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<TodoList> TodoLists { get; set; }
+        DbSet<TodoItem> TodoItems { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
