cc6a9d15d9b866b6ae50236310fbbb44c6d70c7e
Francis Chung <francis.c.chung@gmail.com>
2020-03-28 14:58:51 +1100

    New GetTodoListsQuery

---
+++ CaWorkshop.Application/TodoLists/Queries/GetTodoLists/GetTodoListsQuery.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CaWorkshop.Application.Common.Interfaces;
+using CaWorkshop.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaWorkshop.Application.TodoLists.Queries.GetTodoLists
+{
+
+    public interface IGetTodoListsQuery
+    {
+        Task<List<TodoList>> Handle();
+    }
+
+    public class GetTodoListsQuery : IGetTodoListsQuery
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetTodoListsQuery(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<TodoList>> Handle()
+        {
+            return await _context.TodoLists
+                .Select(l => new TodoList
+                {
+                    Id = l.Id,
+                    Title = l.Title,
+                    Items = l.Items.Select(i => new TodoItem
+                    {
+                        Id = i.Id,
+                        ListId = i.ListId,
+                        Title = i.Title,
+                        Done = i.Done,
+                        Priority = i.Priority,
+                        Note = i.Note
+                    }).ToList()
+                }).ToListAsync();
+        }
+    }
+}
