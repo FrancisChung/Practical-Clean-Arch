45542987b29bf2ab6e8e9e44eaeed18744cf8733
Francis Chung <francis.c.chung@gmail.com>
2020-03-29 16:42:58 +1100

    New CreateTodoListCommand

---
+++ CaWorkshop.Application/TodoLists/Commands/CreateTodoList/CreateTodoListCommand.cs
@@ -0,0 +1,35 @@
+﻿using CaWorkshop.Application.Common.Interfaces;
+using CaWorkshop.Domain.Entities;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CaWorkshop.Application.TodoLists.Commands.CreateTodoList
+{
+    public class CreateTodoListCommand : IRequest<int>
+    {
+        public string Title { get; set; }
+    }
+
+    public class CreateTodoListCommandHandler : IRequestHander<CreateTodoListCommand, int>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public CreateTodoListCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Hander(CreateTodoListCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new TodoList();
+            entity.Title = request.Title;
+
+            _context.TodoLists.Add(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return entity.Id;
+        }
+    }
+}
