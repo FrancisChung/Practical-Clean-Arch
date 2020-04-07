283471df8d30ecfe163b4abc9884232f36e7b2b6
Francis Chung <francis.c.chung@gmail.com>
2020-04-07 10:42:22 +1000

    New CreateTodoItem and Handler

---
+++ CaWorkshop.Application/TodoItems/Commands/CreateTodoItem.cs
@@ -0,0 +1,40 @@
+﻿using CaWorkshop.Application.Common.Interfaces;
+using CaWorkshop.Domain.Entities;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CaWorkshop.Application.TodoItems.Commands
+{
+    public class CreateTodoItem : IRequest<long>
+    {
+        public int ListId { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class CreateTodoItemCommandHandler : IRequestHandler<CreateTodoItem, long>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public CreateTodoItemCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<long> Handle(CreateTodoItem request, CancellationToken cancellationToken)
+        {
+            var entity = new TodoItem
+            {
+                ListId = request.ListId,
+                Title = request.Title,
+                Done = false
+            };
+
+            _context.TodoItems.Add(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+            return entity.Id;
+        }
+    }
+}
