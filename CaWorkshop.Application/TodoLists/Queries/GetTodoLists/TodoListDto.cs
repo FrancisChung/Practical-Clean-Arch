c46824225f3669d485ba6cf89b6b6411dd378325
Francis Chung <francis.c.chung@gmail.com>
2020-04-08 10:26:00 +1000

    TodoListDto added

---
+++ CaWorkshop.Application/TodoLists/Queries/GetTodoLists/TodoListDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CaWorkshop.Application.TodoLists.Queries.GetTodoLists
+{
+    public class TodoListDto
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public IList<TodoItemDto> Items { get; set; }
+    }
+}
