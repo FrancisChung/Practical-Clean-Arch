cbb88cc4f325fa7f1cd044ac031491e035206e47
Francis Chung <francis.c.chung@gmail.com>
2020-04-08 12:54:48 +1000

    New PriorityLevelDto

---
+++ CaWorkshop.Application/TodoLists/Queries/GetTodoLists/PriorityLevelDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaWorkshop.Application.TodoLists.Queries.GetTodoLists
+{
+    public class PriorityLevelDto
+    {
+        public int Value { get; set; }
+
+        public string Name { get; set; }
+    }
+}
