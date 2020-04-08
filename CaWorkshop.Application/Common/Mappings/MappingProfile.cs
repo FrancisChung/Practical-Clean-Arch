9ac6a9582ce9d777b218d8e2aaee942e06c77bbd
Francis Chung <francis.c.chung@gmail.com>
2020-04-09 08:28:51 +1000

    Automatically register all models for Mapping

--- CaWorkshop.Application/Common/Mappings/MappingProfile.cs
+++ CaWorkshop.Application/Common/Mappings/MappingProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
-using CaWorkshop.Application.TodoLists.Queries.GetTodoLists;
-using CaWorkshop.Domain.Entities;
+using System;
+using System.Linq;
+using System.Reflection;
 
 namespace CaWorkshop.Application.Common.Mappings
 {
@@ -8,8 +9,27 @@ namespace CaWorkshop.Application.Common.Mappings
     {
         public MappingProfile()
         {
-            new TodoListDto().Mapping(this);
-            new TodoItemDto().Mapping(this);
+            ApplyMappingsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                .Where(t => t.GetInterfaces().Any(i =>
+                    i.IsGenericType &&
+                    i.GetGenericTypeDefinition() == typeof(IMapFrom<>)))
+                .ToList();
+
+            foreach (var type in types)
+            {
+                var method = type.GetMethod("Mapping") ??
+                             type.GetInterface("IMapFrom`1")
+                                 .GetMethod("Mapping");
+
+                var instance = Activator.CreateInstance(type);
+
+                method?.Invoke(instance, new object[] { this });
+            }
         }
     }
 }
