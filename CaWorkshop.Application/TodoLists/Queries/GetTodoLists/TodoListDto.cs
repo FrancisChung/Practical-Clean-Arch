e876f7922544590b33a920d25d408965883d2c26
Francis Chung <francis.c.chung@gmail.com>
2020-04-09 06:32:52 +1000

    TodoLIstDto to use IMapFrom

--- CaWorkshop.Application/TodoLists/Queries/GetTodoLists/TodoListDto.cs
+++ CaWorkshop.Application/TodoLists/Queries/GetTodoLists/TodoListDto.cs
@@ -3,15 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using AutoMapper;
+using CaWorkshop.Application.Common.Mappings;
 
 namespace CaWorkshop.Application.TodoLists.Queries.GetTodoLists
 {
-    public class TodoListDto
+    public class TodoListDto : IMapFrom<TodoList>
     {
         public int Id { get; set; }
 
         public string Title { get; set; }
 
         public IList<TodoItemDto> Items { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<TodoList, TodoListDto>()
+        }
     }
 }
