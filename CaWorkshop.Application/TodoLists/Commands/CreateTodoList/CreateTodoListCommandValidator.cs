f1573bb7782e681a8908ebff693c64edf513fe19
Francis Chung <francis.c.chung@gmail.com>
2020-04-09 08:43:42 +1000

    FluetValidator added

---
+++ CaWorkshop.Application/TodoLists/Commands/CreateTodoList/CreateTodoListCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace CaWorkshop.Application.TodoLists.Commands.CreateTodoList
+{
+    public class CreateTodoListCommandValidator : AbstractValidator<CreateTodoListCommand>
+    {
+        public CreateTodoListCommandValidator()
+        {
+            RuleFor(v => v.Title)
+                .MaximumLength(240)
+                .NotEmpty();
+        }
+    }
+}
