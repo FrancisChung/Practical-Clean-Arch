4567b64e92ae51072765340cce106f51c1e38c53
Francis Chung <francis.c.chung@gmail.com>
2020-04-09 06:17:17 +1000

    New Automapper Mapping Profile

---
+++ CaWorkshop.Application/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CaWorkshop.Application.TodoLists.Queries.GetTodoLists;
+using CaWorkshop.Domain.Entities;
+
+namespace CaWorkshop.Application.Common.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<TodoList, TodoListDto>();
+
+            CreateMap<TodoItem, TodoItemDto>()
+                .ForMember(d => d.Priority, opt =>
+                    opt.MapFrom(s => (int) s.Priority));
+        }
+    }
+}
