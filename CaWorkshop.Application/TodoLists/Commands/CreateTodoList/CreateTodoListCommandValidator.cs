4969d69b69e5cd52793bdc1d69c35885d19158ca
Francis Chung <francis.c.chung@gmail.com>
2020-04-09 11:47:00 +1000

    Integrating FluentValidation with ASP.NET Core

--- CaWorkshop.Application/TodoLists/Commands/CreateTodoList/CreateTodoListCommandValidator.cs
+++ CaWorkshop.Application/TodoLists/Commands/CreateTodoList/CreateTodoListCommandValidator.cs
@@ -2,18 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using CaWorkshop.Application.Common.Interfaces;
 using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 
 namespace CaWorkshop.Application.TodoLists.Commands.CreateTodoList
 {
     public class CreateTodoListCommandValidator : AbstractValidator<CreateTodoListCommand>
     {
-        public CreateTodoListCommandValidator()
+        private IApplicationDbContext _context;
+
+        public CreateTodoListCommandValidator(IApplicationDbContext context)
         {
+            _context = context;
+
             RuleFor(v => v.Title)
                 .MaximumLength(240)
-                .NotEmpty();
+                .NotEmpty()
+                .MustAsync(BeUniqueTitle)
+                .WithMessage("The Specified Title already exists");
+
+        }
+
+        public async Task<bool> BeUniqueTitle(string title, CancellationToken cancellationToken)
+        {
+            return await _context.TodoLists.AllAsync(l => l.Title != title);
         }
     }
 }
