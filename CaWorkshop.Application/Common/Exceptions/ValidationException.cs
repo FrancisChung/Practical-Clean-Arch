e31e3f1554ba07a62575b2d0525c34104224d5eb
Francis Chung <francis.c.chung@gmail.com>
2020-04-18 14:27:13 +1000

    New Validation Exception

---
+++ CaWorkshop.Application/Common/Exceptions/ValidationException.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CaWorkshop.Application.Common.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException()
+            : base("One or more validation errors occurred.")
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+
+
+
+        public IDictionary<string, string[]> Errors { get; }
+    }
+}
